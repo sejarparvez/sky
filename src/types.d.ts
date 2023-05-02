185b6f752b9ab41e64ae68e1b0c3fc2a8f467891
sejar parvez <sejarparvez@yahoo.com>
2023-05-02 11:20:58 +0600

    type

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,3 +10,7 @@ declare module "*.jpg" {
   const value: any;
   export default value;
 }
+
+declare module '@iconscout/react-unicons/*';
+declare module 'react-scroll'
+declare module 'number-counter';
